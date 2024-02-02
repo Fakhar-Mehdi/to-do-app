e5e55c4276e90dcbd3090a939f4f8c3874d5b001
fakharmehdi2 <fakhar.mehdi@ocloudsolutions.net>
2024-02-02 11:54:52 +0500

    updations done

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { TodoModule } from './todo/todo.module';
 import { ValidationPipe } from '@nestjs/common';
 import mongoose from 'mongoose';
+import { configDotenv } from 'dotenv';
 
 async function bootstrap() {
+  configDotenv();
   const port = process.env.PORT || 3000;
+  const connectionString = process.env.DB_CONNECTION_STRING || '';
   const app = await NestFactory.create(TodoModule);
   app.useGlobalPipes(new ValidationPipe());
-  await mongoose.connect('mongodb://localhost:27017');
+  await mongoose.connect(connectionString);
   await app.listen(port);
   console.log(`Connected to MongoDB.\nServer listening to port ${port}`);
 }
